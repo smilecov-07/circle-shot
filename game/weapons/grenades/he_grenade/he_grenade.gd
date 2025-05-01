8113855636143859d76f6bbcc52a7130bec5b2fd
Summersay415 <summersay415@gmail.com>
2025-05-01 12:26:29 +0700

    Переработка осколочной гранаты (небольшая)

--- game/weapons/grenades/he_grenade/he_grenade.gd
+++ game/weapons/grenades/he_grenade/he_grenade.gd
@@ -2,7 +2,7 @@ extends Grenade
 
 
 func _customize_projectile(projectile: GrenadeProjectile) -> void:
-	for attack: Attack in projectile.get_node(^"Explosion/Attacks").get_children():
-		attack.who = player.id
-		attack.team = player.team
-		attack.damage_multiplier = player.damage_multiplier
+	var attack: Attack = projectile.get_node(^"Explosion/Attack")
+	attack.who = player.id
+	attack.team = player.team
+	attack.damage_multiplier = player.damage_multiplier
